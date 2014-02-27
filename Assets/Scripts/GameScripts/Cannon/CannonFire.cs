83a2693c6c0dbaed665ce89a7eb07d2b280ce9fe
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-02-27 18:33:09 +0000

    * Finally solve the resetting the HumanCannonball

--- Assets/Scripts/GameScripts/Cannon/CannonFire.cs
+++ Assets/Scripts/GameScripts/Cannon/CannonFire.cs
@@ -31,6 +31,18 @@ public class CannonFire : MonoBehaviour {
 	float angle = 0f;
 	// Update is called once per frame
 	void Update () {
+		// Get the bullet from queue, and make it appear inside the cannon
+		if(InputManager.GetIsInputDown())
+		{
+			GameObject oldestBullet = bullets.Peek();
+			// Enable the object so it is visible
+			oldestBullet.SetActive(true);
+			// Make the human inanimate
+			SetStill(oldestBullet.transform);
+			// Copy transform data from Prefab (reset it's body parts correctly)
+			ResetFromPrefab(oldestBullet.transform);
+		}
+
 		// Rotate the sprite to face the mouse when input depressed
 		if(InputManager.GetIsInputPressed())
 		{
@@ -43,6 +55,18 @@ public class CannonFire : MonoBehaviour {
 			angle = Mathf.Atan2(mouseWorldPt.y, mouseWorldPt.x) * Mathf.Rad2Deg;
 			// Rotate about the z axis by angle degrees
 			gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+			
+			GameObject oldestBullet = bullets.Peek();
+			// Reset position of whole object only, not torso (the rest will follow!)
+			Vector2 cannonToMouse = (InputManager.GetCurrentPosition() - new Vector2(gameObject.transform.position.x, gameObject.transform.position.y)).normalized;
+			Vector2 newRelPos = cannonToMouse * spawnRadius;
+			//oldestBullet.transform.position = gameObject.transform.position + new Vector3(newRelPos.x, newRelPos.y, 0f);
+			oldestBullet.transform.FindChild("torso").position = gameObject.transform.position + new Vector3(newRelPos.x, newRelPos.y, 0f);
+			//oldestBullet.transform.transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+			oldestBullet.transform.FindChild("torso").transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+
+			//ResetFromPrefab(oldestBullet.transform);
 		}
 
 		// Release it according to drag power
@@ -52,15 +76,23 @@ public class CannonFire : MonoBehaviour {
 			GameObject oldestBullet = bullets.Dequeue();
 
 			// Reset the bullet - remove force especially, and rotation of object?
-			oldestBullet.rigidbody2D.velocity = Vector2.zero;
-			oldestBullet.rigidbody2D.angularVelocity = 0f;
+			//FIXME: need to push the data all the way downwards
+			/*for(int i = 0; i < oldestBullet.transform.GetChildCount(); ++i)
+			{
+				GameObject Go = oldestBullet.transform.GetChild(i).gameObject;
+				Go.rigidbody2D.velocity = Vector2.zero;
+                Go.rigidbody2D.angularVelocity = 0f;
+				Debug.Log(Go.name);
+			}*/
+			//oldestBullet.rigidbody2D.velocity = Vector2.zero;
+			//oldestBullet.rigidbody2D.angularVelocity = 0f;
 			// Determine spawn point of bullet based on angle and offset
 			Vector2 cannonToMouse = (InputManager.GetCurrentPosition() - new Vector2(gameObject.transform.position.x, gameObject.transform.position.y)).normalized;
 			// fixme: it could spawn not within the range of that angle above
 			//if(angle <= 90f && angle <= 0f)
 			//{
-			Vector2 newRelPos = cannonToMouse * spawnRadius;
-			oldestBullet.transform.position = gameObject.transform.position + new Vector3(newRelPos.x, newRelPos.y, 0f);
+	//		Vector2 newRelPos = cannonToMouse * spawnRadius;
+	//		oldestBullet.transform.FindChild("torso").position = gameObject.transform.position + new Vector3(newRelPos.x, newRelPos.y, 0f);
 			//oldestBullet.transform.rotation = Quaternion.Euler(cannonToMouse.x, cannonToMouse.y, 0f);//Quaternion.LookRotation(new Vector3(cannonToMouse.x, cannonToMouse.y, Camera.main.transform.position.z));
 			//oldestBullet.transform.rotation = Quaternion.Euler(cannonToMouse.x, 0f, cannonToMouse.y);//Quaternion.LookRotation(new Vector3(cannonToMouse.x, cannonToMouse.y, Camera.main.transform.position.z));
 			//oldestBullet.transform.eulerAngles = new Vector3(cannonToMouse.x, 0f, cannonToMouse.y);
@@ -68,18 +100,88 @@ public class CannonFire : MonoBehaviour {
 			//newRotation.x = 0;
 			//newRotation.y = 0;
 			//oldestBullet.transform.rotation = Quaternion.Euler(newRotation);
-			oldestBullet.transform.rotation = Quaternion.Euler(0f, 0f, angle);
-			Debug.Log(cannonToMouse.ToString());
+	//		oldestBullet.transform.FindChild("torso").transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+	//		Debug.Log(cannonToMouse.ToString());
 			//oldestBullet.transform.Rotate(new Vector3(cannonToMouse.x, cannonToMouse.y, 0f), Space.Self);
 			//}
 			// Fire the bullet - in the direction from Cannon to Mouse
-			oldestBullet.SetActive(true);
+			//oldestBullet.SetActive(true);
+			SetMoving(oldestBullet.transform);
 			float power = Mathf.Max(InputManager.GetCurrentDragOffset().magnitude, MIN_POWER);
-			oldestBullet.rigidbody2D.AddForce(cannonToMouse * power);
+			//oldestBullet.rigidbody2D.AddForce(cannonToMouse * power);
+			//oldestBullet.GetComponentInChildren<Rigidbody2D>().rigidbody2D.AddForce(cannonToMouse * power);
+			oldestBullet.transform.FindChild("torso").rigidbody2D.AddForce(cannonToMouse * power);// * 20);
+			// Set is kinematic so the body parts can fling around!
 
 			// Insert oldest bullet to the back as the freshest
 			bullets.Enqueue(oldestBullet);
-		}
+		}	
+	}
 	
+	// Loop through all the nest child recursively
+	void SetStill(Transform rootTransform)
+	{
+		GameObject go;
+		for(int i = 0; i < rootTransform.childCount; ++i)
+		{
+			go = rootTransform.GetChild(i).gameObject;
+			// Disable collision
+			go.collider2D.enabled = false;
+			// Reset forces
+			go.rigidbody2D.velocity = Vector2.zero;
+			go.rigidbody2D.angularVelocity = 0f;
+			// Make sure it isn't affected by physics
+			go.rigidbody2D.isKinematic = true;
+			// Repeat for all child
+			SetStill(go.transform);
+		}
+	}
+	void SetMoving(Transform rootTransform)
+	{
+		GameObject go;
+		for(int i = 0; i < rootTransform.childCount; ++i)
+		{
+			go = rootTransform.GetChild(i).gameObject;
+			// Re-Enable collision
+			go.collider2D.enabled = true;
+			// Make sure it is affected by physics
+			go.rigidbody2D.isKinematic = false;
+			// Repeat for all child
+			SetMoving(go.transform);
+		}
+	}
+
+	void ResetFromPrefab(Transform rootTransform)
+	{
+		/*GameObject go;
+		for(int i = 0; i < rootTransform.GetChildCount(); ++i)
+		{
+			go = rootTransform.GetChild(i).gameObject;
+			// Re-Enable collision
+			go.collider2D.enabled = true;
+			// Make sure it is affected by physics
+			go.rigidbody2D.isKinematic = false;
+			// Repeat for all child
+			for(int i = 0; i < rootTransform.GetChildCount(); ++i)
+			{
+			}
+		}*/
+		
+		Transform[] allChildren = rootTransform.GetComponentsInChildren<Transform>();
+		Transform[] transforms = BulletPrefab.GetComponentsInChildren<Transform>();
+		if(transforms.Length != allChildren.Length)
+		{
+			Debug.Break();
+		}
+		else
+		{
+			Debug.Log(transforms.Length.ToString());
+		}
+		for(int i = 0; i < allChildren.Length; ++i)
+		{
+			// do whatever with child transform here
+			allChildren[i].position = transforms[i].position;
+			allChildren[i].rotation = transforms[i].rotation;
+		}
 	}
 }
