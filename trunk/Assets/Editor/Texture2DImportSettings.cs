0c6c1e96c50bb67fc696cbbcce10b872cf70d8de
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-03-05 18:46:14 +0000

    * Cleanup Texture2DImportSettings.cs and a failed attempted to manually set pivotpoint via script in game

--- trunk/Assets/Editor/Texture2DImportSettings.cs
+++ trunk/Assets/Editor/Texture2DImportSettings.cs
@@ -64,99 +64,6 @@ public class Texture2DImportSettings : AssetPostprocessor  {
 		}		
 	}
 
-	void OnPostprocessTextureDONOTCALLTHIS(Texture2D texture)
-	{
-		Debug.LogError("aerdere");
-		if(!assetImporter.userData.Equals("loadedBefore"))
-		{
-			// Totally done with custom processing
-			// Set userdata to loadBefore
-			assetImporter.userData = "loadedBefore";	// we check this to make sure we don't overwrite existing changes
-			
-//			AssetDatabase.SaveAssets();
-			EditorUtility.SetDirty(assetImporter);
-			AssetDatabase.Refresh();	// This will trigger the whole postprocess on the same asset once again - not good. But we have no choice.
-			
-		
-			TextureImporter textureImporter = assetImporter as TextureImporter;
-			string fullPath = textureImporter.assetPath;
-
-			// Getting the full path to the asset, but just the folder path
-			int pos = fullPath.LastIndexOf("/");
-			string fullPrefabDirectory = fullPath.Remove(pos + 1);
-
-			// Get just the filename (excluding extension)
-			string filename = fullPath.Substring(pos + 1);
-			filename = filename.Remove(filename.LastIndexOf("."));
-			// todo: we might have to make use of these function if this were to work on MacOS?
-			//string fileName = Path.GetFileNameWithoutExtension(fullPath);
-
-			string fullPrefabPath = fullPrefabDirectory + filename + ".prefab";
-//			AssetDatabase.SaveAssets();
-			EditorUtility.SetDirty(assetImporter);
-			AssetDatabase.Refresh();	// This will trigger the whole postprocess on the same asset once again - not good. But we have no choice.
-
-			// Check if prefab already exists in destination folder
-			if(AssetDatabase.LoadAssetAtPath(fullPrefabPath, (typeof(GameObject))) as GameObject)
-			{
-				Debug.Log("Prefab already exists! " + fullPrefabPath);
-				if(EditorUtility.DisplayDialog("Override existing prefab?",
-				                            "Are you sure you want to create a new prefab? Existing prefabs instances in scene will be messed up!" +
-				                            "\n\nSelect Do not Create if you have no idea what is going on", "Overwrite Prefab", "Cancel Action"))
-				{
-					//CreatePrefab(dir, filename);
-					/*http://docs.unity3d.com/Documentation/ScriptReference/PrefabUtility.CreateEmptyPrefab.html
-		http://answers.unity3d.com/questions/437431/why-does-createemptyprefab-return-null.html
-				http://answers.unity3d.com/questions/8633/how-do-i-programmatically-assign-a-gameobject-to-a.html
-					http://answers.unity3d.com/questions/8633/how-do-i-programmatically-assign-a-gameobject-to-a.html*/
-					//EditorUtility.SetDirty(assetImporter);
-				}
-			}
-			else
-			{
-				{
-					if(EditorUtility.DisplayDialog("Create prefab from asset",
-					                               "Do you want to create a new prefab automatically for asset?\n\n" +
-					                               fullPrefabPath + 
-					                               "\n\nSelect \"Do not Create\" if you have no idea.",
-					                               "Create Prefab", "Do Not Create"))
-					{
-
-						// Replace first occurance of Sprites to Prefabs
-						//string newPrefabDirectory = 
-						//pos = fullPrefabDirectory.IndexOf("Assets/Sprites/");
-						string newPrefabDirectory = fullPrefabDirectory.Replace("Assets/Sprites/", "Assets/Prefabs/");
-						string newPrefabPath = newPrefabDirectory + filename + ".prefab";
-						if(EditorUtility.DisplayDialog("Move prefab?",
-						                               "Do you want to move the newly created prefab from\n\"" + fullPrefabPath + "\"\nto\n\"" + newPrefabPath + "\"\n?",
-						                               "Yes please", "No, leave it as it is"))
-						{
-							if(!Directory.Exists(newPrefabDirectory))
-							{
-								Directory.CreateDirectory(newPrefabDirectory);
-								// Making sure the folder shows up in Project panel
-								// This will import any assets that have changed their content modification data or have been added-removed to the project folder.
-								AssetDatabase.Refresh();	// This will trigger the whole postprocess on the same asset once again - not good. But we have no choice.
-							}
-							//Debug.Log("Created prefab: " + newPrefabPath);
-							// EditorApplication.delayCall += CreatePrefab(AssetDatabase.LoadAssetAtPath(fullPath, typeof(Texture2D)) as Texture2D, newPrefabPath);
-						}
-						else
-						{
-							Debug.Log("Created prefab: " + fullPrefabPath);
-							//CreatePrefab(AssetDatabase.LoadAssetAtPath(fullPath, typeof(Texture2D)) as Texture2D, fullPrefabPath);
-
-//							createPrefabCB = new EditorApplication.CallbackFunction(createPrefabCB, );
-
-//							EditorApplication.delayCall += createPrefabCB;
-						}
-
-						AssetDatabase.Refresh();
-					}
-				}
-			}
-		}
-	}
 	/// <summary>
 	/// Handles when ANY asset is imported, deleted, or moved.  Each parameter is the full path of the asset, including filename and extension.
 	/// </summary>
@@ -199,38 +106,11 @@ public class Texture2DImportSettings : AssetPostprocessor  {
 			return;
 		}
 		ai.userData += "prefabPrompted|";
-		Debug.LogWarning("Sanity check for 'prefabPrompted' string: " + AssetImporter.GetAtPath(fullPath).userData);
-
-		Texture2D texture = Resources.LoadAssetAtPath(fullPath, typeof(Texture2D)) as Texture2D;
-		if(texture == null)
-		{
-			Debug.LogError("Unable to load texture for prefab creation!");
-			return;
-		}
-		Sprite sprite = Resources.LoadAssetAtPath(fullPath, typeof(Sprite)) as Sprite;
-		if(sprite == null)
-		{
-			Debug.LogError("Unable to load sprite for prefab creation!");
-			return;
-		}
-		Object obj = AssetDatabase.LoadMainAssetAtPath(fullPath);
-		if(AssetDatabase.IsMainAsset(obj))
-		{
-			Debug.Log("ismainssset");
-		}
-		if(AssetDatabase.IsSubAsset(sprite))
-		{
-			Debug.Log("IsSubAsset");
-		}
-		if(AssetDatabase.Contains(sprite))
-		{
-			Debug.Log("is an asset file");
-		}
 
 		int pos = fullPath.LastIndexOf("/");
 		string fullDirectory = fullPath.Remove(pos + 1);
 		string fileName = Path.GetFileNameWithoutExtension(fullPath);
-		string fullWithoutExtension = fullDirectory + fileName;
+		//string fullWithoutExtension = fullDirectory + fileName;
 
 		// Ask if they want the prefab to be created
 		if(EditorUtility.DisplayDialog("Create prefab from asset",
@@ -240,9 +120,9 @@ public class Texture2DImportSettings : AssetPostprocessor  {
 		                               "Create Prefab", "Do Not Create"))
 		{
 			// Create prefab at Sprites folder
-			string fullPrefabPath = fullWithoutExtension + ".prefab";
-			GeneratePrefab(texture, fullPrefabPath);
+			GeneratePrefab(fullPath);
 
+			string fullPrefabPath = fullPath.Replace(".png", ".prefab");//fullWithoutExtension + ".prefab";
 			string newPrefabDirectory = fullDirectory.Replace("Assets/Sprites/", "Assets/Prefabs/");
 			string newPrefabPath = newPrefabDirectory + fileName + ".prefab";
 			if(EditorUtility.DisplayDialog("Move prefab?",
@@ -258,49 +138,31 @@ public class Texture2DImportSettings : AssetPostprocessor  {
 				AssetDatabase.MoveAsset(fullPrefabPath, newPrefabPath);
 			}
 		}
-
-
 	}
 
-	static void GeneratePrefab(Texture2D texture, string fullPath)
+	static void GeneratePrefab(string fullPath)
 	{
-		Debug.LogWarning("Sanity check for 'texture name' string: " + texture.name);
 		// Create and prepare your game object.
 		GameObject go = new GameObject(fullPath);
-		//EditorUtility.SetDirty(go);
 		SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
 		sr.sortingLayerName = "GameObjects";
 
-		string omg = fullPath.Replace(".prefab", ".png");
-
-		Sprite tmpSprite = new Sprite();
-		Object obj = AssetDatabase.LoadMainAssetAtPath(omg);
-		Debug.Log("obj name:" + obj.name);
-
-		// fail
-		//Texture2D texturetemp1 = Resources.LoadAssetAtPath(omg, typeof(Texture2D)) as Texture2D;
-		//Texture2D texturetemp2 = AssetDatabase.LoadAssetAtPath(omg, typeof(Texture2D)) as Texture2D;
-
+		Texture2D texture = AssetDatabase.LoadAssetAtPath(fullPath, typeof(Texture2D)) as Texture2D;
 		if(fullPath.Contains("Levels/Ground"))
 		{
-			tmpSprite = Sprite.Create(texture, new Rect(0,0,texture.width,texture.height), new Vector2 (0.5f, 0f));
+			sr.sprite = Sprite.Create(texture, new Rect(0,0,texture.width,texture.height), new Vector2 (0.5f, 0f));
 			go.transform.position = new Vector3(0f, -5f, go.transform.position.z);
 			go.tag = "Obstacle";
+			// Workaround for not being able to set the pivot point via Sprite.Create
+			go.AddComponent<SetSpritePivot>().texture = texture;
 		}
-		else
+		//else
 		{
-			tmpSprite = Sprite.Create(texture, new Rect(0,0,texture.width,texture.height), new Vector2 (0.5f, 0.0f));
+			//sr.sprite = Resources.LoadAssetAtPath(omg, typeof(Sprite)) as Sprite;
+			sr.sprite = AssetDatabase.LoadAssetAtPath(fullPath, typeof(Sprite)) as Sprite;
+			//sr.sprite = Sprite.Create(testing, new Rect(0,0,testing.width,testing.height), new Vector2(0.5f, 0f));
+			//sr.sprite = Sprite.Create(sr.sprite.texture, new Rect(0,0,sr.sprite.texture.width,sr.sprite.texture.height), new Vector2(0.5f, 0f));
 		}
-
-		// FK UPPPPPPP HEREE HOWOWOWHWHOWOW
-		//EditorUtility.SetDirty(tmpSprite);
-		//sr.sprite = tmpSprite;
-		//sr.sprite = Resources.LoadAssetAtPath(omg, typeof(Sprite)) as Sprite;
-		sr.sprite = AssetDatabase.LoadAssetAtPath(omg, typeof(Sprite)) as Sprite;
-		//Texture2D testing = AssetDatabase.LoadAssetAtPath(omg, typeof(Texture2D)) as Texture2D;
-		//sr.sprite = Sprite.Create(testing, new Rect(0,0,testing.width,testing.height), new Vector2(0.5f, 0f));
-		//sr.sprite = Sprite.Create(sr.sprite.texture, new Rect(0,0,sr.sprite.texture.width,sr.sprite.texture.height), new Vector2(0.5f, 0f));
-		Debug.Log( sr.sprite.bounds.center.normalized );
 
 		// Check if the object is obstacle
 		if(fullPath.Contains("Levels/Obstacles"))
@@ -315,21 +177,14 @@ public class Texture2DImportSettings : AssetPostprocessor  {
 			sr.sortingLayerName = "Decorations";
 			sr.sortingOrder = -3;
 		}
-		/*
-		SpriteRenderer rr = go.GetComponent<SpriteRenderer>();
-		rr.sprite = new Sprite()
-		Debug.Log(texture.name + " " + go.GetComponent<SpriteRenderer>().sprite.name);
-*/
-		//EditorUtility.SetDirty(go);
-
+		
+		string fullPrefabPath = fullPath.Replace(".png", ".prefab");//fullWithoutExtension + ".prefab";
 		// Create prefab from object.
-		PrefabUtility.CreatePrefab(fullPath, go, ReplacePrefabOptions.ConnectToPrefab);
+		PrefabUtility.CreatePrefab(fullPrefabPath, go, ReplacePrefabOptions.ConnectToPrefab);
 		// or ReplacePrefabOptions.ReplaceNameBased?
 
 		//Destroy GameObject
 		GameObject.DestroyImmediate (go);
-
-		//AssetDatabase.Refresh();
 	}
 
 	// http://forum.unity3d.com/threads/165295-Getting-original-size-of-texture-asset-in-pixels
---
+++ trunk/Assets/SetSpritePivot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This class is used only for Texture2DImportSettings.cs
+public class SetSpritePivot : MonoBehaviour {
+
+	public Texture2D texture;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0f), 72f);
+		Destroy(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
