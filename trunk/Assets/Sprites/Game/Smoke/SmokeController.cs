119a490324e5acdaef179791417c6054a1863a7f
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-03-08 12:29:03 +0000

    + Assets for smake for cannon fire

---
+++ trunk/Assets/Sprites/Game/Smoke/SmokeController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmokeController : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(anim)
+		{
+
+		}
+	}
+}
