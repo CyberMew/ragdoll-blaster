81ce63b42b6513a0174bf6a0959fecc7030401bd
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-03-10 18:23:31 +0000

    * Lava are now obstacledestroy tag which can destroy TargetBoard
    - Remove Smoke/Smoke45Frames_45.prefab Animation Component since it was causing warnings even though unchecked/disabled
    
    + Objects touching the lava will just sink, but gravity not changed yet
    
    * Lava touching TargetBoard will instantly restart the game

--- trunk/Assets/Scripts/GameScripts/Obstacles/Lava.cs
+++ trunk/Assets/Scripts/GameScripts/Obstacles/Lava.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class Lava : MonoBehaviour {
 
+	private bool isTriggered;
+
 	// Use this for initialization
 	void Start () {
-	
+		isTriggered = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(isTriggered == false && (other.CompareTag("Bullet") || other.CompareTag("Target")))
+		{
+			Debug.Log("Something triggered the LAVA!");
+			isTriggered = true;
+
+			// Remove forces and let it sink down slowly
+			other.rigidbody2D.velocity = Vector2.zero;
+			other.rigidbody2D.angularVelocity = 0f;
+
+		}
+	}
+	/*
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//if(other.gameObject.CompareTag("Bullet"))
+		{
+			Debug.Log("Something collided the LAVA!");
+		}
+	}*/
 }
