7fefb3f83799b4c3adc0b5e225903a6b7004b170
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-03-11 07:23:29 +0000

    + Level7.unity and night background
    + Still working on the CaveTeleport.cs

---
+++ Assets/CaveTeleport.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveTeleport : MonoBehaviour {
+
+	public GameObject caveOut;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Bullet"))
+		{
+			other.transform.position = caveOut.transform.position;
+		}
+	}
+}
