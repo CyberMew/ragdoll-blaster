1099e7cd90ce743dc889716c456200090d360c97
cybermew@gmail.com <cybermew@gmail.com@2b5e59d8-a599-e10b-9c03-d571bba57674>
2014-04-06 08:05:51 +0000

    * Fixed an issue where we used a blank head everytime when the profile picture isn't downloaded yet.

--- Assets/Scripts/GameScripts/Facebook/FacebookHead.cs
+++ Assets/Scripts/GameScripts/Facebook/FacebookHead.cs
@@ -5,7 +5,7 @@ public class FacebookHead : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if(FBUtils.IsLoggedIn)
+		if(FBUtils.IsLoggedIn && User.profilePicture.width > 1)
 		{
 			GetComponent<SpriteRenderer>().sprite = Sprite.Create(User.profilePicture, new Rect(0,0,User.profilePicture.width,User.profilePicture.height), new Vector2(0.5f,0.5f), 72f);
 			transform.localScale *= 0.75f;
